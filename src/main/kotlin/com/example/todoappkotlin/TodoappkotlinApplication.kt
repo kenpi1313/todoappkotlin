49e4808ddda260ea8530f7dd139ec8b8fda88453
kawaharakenta <kawaharakenta@team-lab.com>
2020-08-09 21:00:33 +0900

    initial commit

---
+++ src/main/kotlin/com/example/todoappkotlin/TodoappkotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.todoappkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoappkotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoappkotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/todoappkotlin/TodoappkotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.todoappkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoappkotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
